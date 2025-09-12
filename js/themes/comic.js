b74f958cc20f2a5ad69aab8a24600f4dbdd308f7
nikuhkid <46968851+nikuhkid@users.noreply.github.com>
2025-09-12 12:12:41 +0100

    Update comic.js
    Signed-off-by: nikuhkid <46968851+nikuhkid@users.noreply.github.com>

--- js/themes/comic.js
+++ js/themes/comic.js
@@ -1,16 +1,19 @@
-console.log("Comic theme active.");
+// comic.js - click produces comic assets (pow, zap, splash)
+(function(){
+  const assets = ['assets/pow.png','assets/zap.png','assets/splash.png'];
+  function onClick(e){
+    const img = document.createElement('img');
+    img.src = assets[Math.floor(Math.random()*assets.length)];
+    img.className = 'comic-sprite';
+    img.style.left = (e.pageX - 50) + 'px';
+    img.style.top = (e.pageY - 50) + 'px';
+    document.body.appendChild(img);
+    setTimeout(()=> { img.classList.add('fade-out'); setTimeout(()=> img.remove(), 400); }, 650);
+  }
+  document.addEventListener('click', onClick);
 
-document.body.addEventListener("click", (e) => {
-  const pow = document.createElement("div");
-  pow.innerText = "POW!";
-  pow.style.position = "absolute";
-  pow.style.left = e.pageX + "px";
-  pow.style.top = e.pageY + "px";
-  pow.style.fontSize = "20px";
-  pow.style.fontWeight = "bold";
-  pow.style.color = "red";
-  pow.style.zIndex = 9999;
-  document.body.appendChild(pow);
-
-  setTimeout(() => pow.remove(), 800);
-});
+  window.__themeCleanup = function(){
+    document.removeEventListener('click', onClick);
+    document.querySelectorAll('.comic-sprite').forEach(n=>n.remove());
+  };
+})();
